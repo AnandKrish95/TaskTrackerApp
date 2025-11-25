a6b4f7b3740102e499945d1d9c96b2dd9efc4fc6
AnandKrish95 <anankrish95@gmail.com>
2025-11-26 01:23:34 +0530

    Add files via upload

---
+++ src/test/java/com/ttr/TastTrackerApp/TastTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttr.TastTrackerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TastTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
